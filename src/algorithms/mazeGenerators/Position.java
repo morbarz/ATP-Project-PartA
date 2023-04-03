87b24a714eefa651558506dd04a82c688e3cef39
morbarz <morbarz@post.bgu.ac.il>
2023-04-03 19:19:13 +0300

    changed nowwwwwwww

--- src/algorithms/mazeGenerators/Position.java
+++ src/algorithms/mazeGenerators/Position.java
@@ -1,4 +1,20 @@
 package algorithms.mazeGenerators;
 
 public class Position {
+    int rowX; // row
+    int colY; // columns
+
+    public int getColumnIndex() {
+        return colY;
+    }
+    public int getRowIndex(){
+        return rowX;
+    }
+    public void position(int rowx , int coly){ // constructor
+        //Position newP = new Position();
+        this.rowX = rowx;
+        this.colY = coly;
+    }
+
+
 }
