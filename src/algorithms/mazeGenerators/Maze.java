4ff490892e37f72d6f1b1d8ae89b1bbe22b5d507
NivSampson <129731047+NivSampson@users.noreply.github.com>
2023-04-03 19:15:18 +0300

    change

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -4,20 +4,26 @@ public class Maze {
     int columns;
     int rows;
     int[][] maze;
-    public void MazeGenerator(int row, int col ){//constructor
-        this.rows=row;
-        this.columns=col;
+
+    public void MazeGenerator(int row, int col) {//constructor
+        this.rows = row;
+        this.columns = col;
         this.maze = new int[row][col];
 
     }
-    public void Print(Maze MazeToPrint){
+
+    public void Print(Maze MazeToPrint) {
+        System.out.println("{"+ this.rows +"," + this.columns + "}" );
 
     }
-    public Position getStartPosition(Maze Smaze){
+
+    public Position getStartPosition(Maze Smaze) {
 
     }
-    public Position getGoalPosition(Maze Gmaze){
 
+    public Position getGoalPosition(Maze Gmaze) {
 
 
     }
+}
+
