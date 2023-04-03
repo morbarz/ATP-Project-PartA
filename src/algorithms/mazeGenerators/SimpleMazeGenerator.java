e5b877f66474d47d9f712537a2e8977b54e6ae03
NivSampson <129731047+NivSampson@users.noreply.github.com>
2023-04-03 13:11:59 +0300

    change

--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -1,9 +1,19 @@
 package algorithms.mazeGenerators;
-
-public class SimpleMazeGenerator extends AMazeGenerator{
+import java.util.Random;
+public class SimpleMazeGenerator extends AMazeGenerator {
     @Override
     public Maze generate(int row, int col) {
-        Object ob;
-        return null;
-    }
+        Maze newMaze = new Maze();
+        newMaze.MazeGenerator(row, col);
+        Random random = new Random();
+        int randomNUmber = random.nextInt(2);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                newMaze.maze[i][j] = randomNUmber;
+
+            }
+
+        }
+
+        return newMaze;   }
 }
