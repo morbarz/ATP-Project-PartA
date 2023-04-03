c3344a084899562bf2caddd102e732f186b7f459
morbarz <morbarz@post.bgu.ac.il>
2023-04-03 12:39:12 +0300

    change

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -4,10 +4,11 @@ public class Maze {
     int columns;
     int rows;
     int[][] maze;
-    public void maze(int row, int col ){//constructor
-        this.maze = new int[row][col];
+    public void MazeGenerator(int row, int col ){//constructor
         this.rows=row;
         this.columns=col;
+        this.maze = new int[row][col];
+
     }
     ///Maze MazeGenerator = new Maze(int )
 
