76b4cdcf1fcf5205850c41b56ed57775a363f8cc
morbarz <morbarz@post.bgu.ac.il>
2023-04-03 12:39:59 +0300

    change

--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -3,6 +3,6 @@ package algorithms.mazeGenerators;
 public class SimpleMazeGenerator extends AMazeGenerator{
     @Override
     public Maze generate(int row, int col) {
-        return null;
+        return ;
     }
 }
